c0849e8fb6709620eb3b62cddb18b799c093d2ae
MonsterGameDev <per@brf.dk>
2022-08-08 12:09:53 +0200

    try yml script

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -3,6 +3,6 @@ import "./home.scss";
 const body = document.querySelector("body");
 
 const header2 = document.createElement("h2");
-header2.innerHTML = "Here I am H2";
+header2.innerHTML = "Here I am H2xx";
 
 body!.appendChild(header2);
