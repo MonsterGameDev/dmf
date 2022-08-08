45df58df7e8709db2f2f2c8c29883ceff887512f
MonsterGameDev <per@brf.dk>
2022-08-08 12:21:17 +0200

    temp

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -3,6 +3,6 @@ import "./home.scss";
 const body = document.querySelector("body");
 
 const header2 = document.createElement("h2");
-header2.innerHTML = "Here I am H2xx";
+header2.innerHTML = "Here I am H2";
 
 body!.appendChild(header2);
