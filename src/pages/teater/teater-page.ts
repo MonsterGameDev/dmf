f452e208c4e801ad0a3b24c7f58cf6daf21c5b59
MonsterGameDev <per@brf.dk>
2022-08-08 15:22:39 +0200

    teater-page init

---
+++ src/pages/teater/teater-page.ts
@@ -0,0 +1,7 @@
+import "./teater-page.scss";
+import "./../../components/theater/stage/stage.ts";
+
+const body = document.querySelector("body");
+const page = document.querySelector(".page");
+const stageSection = document.querySelector("stage-section");
+const stageContainer = document.querySelector("stage-section__container");
