db03e525579845061143b706be0ee486c79e23ef
MonsterGameDev <per@brf.dk>
2022-08-09 13:01:29 +0200

    removing sass and ts files

--- src/pages/teater/teater-page.ts
+++
@@ -1,7 +0,0 @@
-import "./teater-page.scss";
-import "./../../components/theater/stage/stage.ts";
-
-const body = document.querySelector("body");
-const page = document.querySelector(".page");
-const stageSection = document.querySelector("stage-section");
-const stageContainer = document.querySelector("stage-section__container");
