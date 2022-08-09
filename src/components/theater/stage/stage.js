ef11c2ded3d61da7238f968a58db5ad2d1d0f8ab
MonsterGameDev <per@brf.dk>
2022-08-09 16:20:35 +0200

    step1

--- src/components/theater/stage/stage.js
+++ src/components/theater/stage/stage.js
@@ -175,7 +175,12 @@ class StageComponent extends HTMLElement {
     if (!this._isOpen) return;
 
     if (this.hasTouchScreen) {
-      alert("target: " + e.touches[0].target);
+      try {
+        alert("target: " + e.touches[0].target.offsetLeft);
+      } catch {
+        alert('Error')
+      }
+
       e.offsetX = e.touches[0].pageX - e.touches[0].target.offsetLeft;
       e.offsetY = e.touches[0].pageY - e.touches[0].target.offsetTop;
     }
