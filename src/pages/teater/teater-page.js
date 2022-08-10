14694aac9bb68aac0dcda8f0d86b0d46b40cb56f
MonsterGameDev <per@brf.dk>
2022-08-10 23:25:25 +0200

    cleanup

--- src/pages/teater/teater-page.js
+++ src/pages/teater/teater-page.js
@@ -3,74 +3,6 @@ import "./../../components/theater/stage/stage.js";
 import "./../../components/form-components/radio-button-group-component.js"
 import StageRetrievalService from "../../shared/stages/stage-retrieval.service"; "../../shared/stages/stage-retrieval.service.js";
 
-
-
-// import Bagtaeppe from "./../../img/stages/pariser-opera/D-467.webp";
-// import Mellemtaeppe from "./../../img/stages/pariser-opera/D-469.webp";
-// import Fortaeppe from "./../../img/stages/pariser-opera/D-470.webp";
-
-// import Bagtaeppe_RoedStue from "./../../img/stages/roed-stue/D-047.webp";
-// import Mellemtaeppe_RoedStue from "./../../img/stages/roed-stue/D-048-mellem.webp";
-// import Fortaeppe_RoedStue from "./../../img/stages/roed-stue/D-048-for.webp";
-
-
-// import Bagtaeppe_GlGade from "./../../img/stages/gammel-gade-01/D-022.webp";
-// import Mellemtaeppe_GlGade from "./../../img/stages/gammel-gade-01/D-011-mellem.webp";
-// import Fortaeppe_GlGade from "./../../img/stages/gammel-gade-01/D-011-for.webp";
-
-
-// const stageLayers_GlGade = [
-//   {
-//     imgSrc: Bagtaeppe_GlGade,
-//     altText: "bagtaeppe",
-//     zIndex: 1,
-//   },
-//   {
-//     imgSrc: Mellemtaeppe_GlGade,
-//     altText: "mellemtaeppe",
-//     zIndex: 2,
-//   },
-//   {
-//     imgSrc: Fortaeppe_GlGade,
-//     altText: "fortæppe",
-//     zIndex: 3,
-//   },
-// ];
-// const stageLayers_RoedStue = [
-//   {
-//     imgSrc: Bagtaeppe_RoedStue,
-//     altText: "bagtaeppe",
-//     zIndex: 1,
-//   },
-//   {
-//     imgSrc: Mellemtaeppe_RoedStue,
-//     altText: "mellemtaeppe",
-//     zIndex: 2,
-//   },
-//   {
-//     imgSrc: Fortaeppe_RoedStue,
-//     altText: "fortæppe",
-//     zIndex: 3,
-//   },
-// ];
-// const stageLayers = [
-//   {
-//     imgSrc: Bagtaeppe,
-//     altText: "bagtaeppe",
-//     zIndex: 1,
-//   },
-//   {
-//     imgSrc: Mellemtaeppe,
-//     altText: "mellemtaeppe",
-//     zIndex: 2,
-//   },
-//   {
-//     imgSrc: Fortaeppe,
-//     altText: "fortæppe",
-//     zIndex: 3,
-//   },
-// ];
-
 const _availableStages = [
   {
     id: 'pariseropera',
@@ -88,7 +20,6 @@ const _availableStages = [
     label: 'Gammel gade'
   },
 ];
-
 const groupConfig = {
   groupHeading: 'Vælg en scene',
   fieldName: 'stageSelector',
@@ -97,16 +28,8 @@ const groupConfig = {
 
 const stageService = new StageRetrievalService();
 
-const body = document.querySelector("body");
-const page = document.querySelector(".page");
-const stageSection = document.querySelector(".stage-section");
-const stageContainer = document.querySelector(".stage-section__container");
-const codeBlock = document.querySelector('.code-example');
-
 const phStage = document.querySelector("ph-stage");
-//phStage.layers = stageLayers;
-//phStage.layers = stageLayers_RoedStue;
-phStage.layers = stageService.getStageById('pariseropera');//stageLayers_GlGade;
+phStage.layers = stageService.getStageById('pariseropera');
 
 const stageSelector = document.querySelector('ph-radio-button-group');
 stageSelector.groupConfig = groupConfig;
