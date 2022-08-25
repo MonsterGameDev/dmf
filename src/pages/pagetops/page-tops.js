9a69e83ff845680b17dfbee67f15e31737750f0d
MonsterGameDev <per@brf.dk>
2022-08-25 11:40:27 +0200

    page-tops presentation-page added new webcomponents

--- src/pages/pagetops/page-tops.js
+++ src/pages/pagetops/page-tops.js
@@ -1,1 +1,15 @@
-import './../../components/page-tops/indian-jungle-parallax.js';
+import './../../components/pagetops/indian-jungle-parallax.js';
+import './../../components/pagetops/indian-praries-parallax.js';
+import './page-tops.scss';
+
+const contentContainer = document.querySelector('.content-container');
+const phIndianJungleParallax = document.querySelector('ph-indian-jungle-parallax#first')
+contentContainer.addEventListener('scroll', () => {
+    phIndianJungleParallax.setAttribute('scroll-top', contentContainer.scrollTop)
+});
+
+const contentContainer_2 = document.querySelector('.content-container-2');
+const phIndianJungleParallax_2 = document.querySelector('ph-indian-prarie-parallax#second')
+contentContainer_2.addEventListener('scroll', () => {
+    phIndianJungleParallax_2.setAttribute('scroll-top', contentContainer_2.scrollTop)
+});
