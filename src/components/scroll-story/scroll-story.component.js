d44c2485066e32acd866d9ea66963066fb042e98
MonsterGameDev <per@brf.dk>
2022-09-03 13:57:43 +0200

    scroll-story test

--- src/components/scroll-story/scroll-story.component.js
+++ src/components/scroll-story/scroll-story.component.js
@@ -1,16 +1,16 @@
 const scrollStoryTemplate = document.createElement('template');
 scrollStoryTemplate.innerHTML = `
     <style>
+
     * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
-  .container {
+  .inner-container {
     width: 100%;
     height: 100vh;
-    background-image: url("./img//perron.webp");
     background-size: cover;
     background-position: center center;
     position: relative;
@@ -29,9 +29,10 @@ scrollStoryTemplate.innerHTML = `
   .text {
     position: absolute;
     text-transform: uppercase;
-    font-size: 20rem;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 25rem;
+    font-weight: 900;
     color: white;
+    font-family: var(--font-family);
   }
 
   .text-fantasi {
@@ -65,37 +66,71 @@ scrollStoryTemplate.innerHTML = `
 `;
 
 class ScrollStoryComponent extends HTMLElement {
-    static get observedAttributes() {
-        return [];
-    }
+  static get observedAttributes() { return ['background-image', '']; }
 
-    get config() {
-        return this._config
-    }
+  get config() {
+    return this._config
+  }
 
-    set config(val) {
-        this._config = val;
-        this.render(val)
-    }
+  set config(val) {
+    this._config = val;
+    console.log(this._config);
+    this.render();
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const templateContent = scrollStoryTemplate.content;
+    this.shadowRoot.appendChild(templateContent.cloneNode(true));
+
+    this.container = this.shadowRoot.querySelector('.inner-container');
 
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+    this.textFantasi = this.shadowRoot.querySelector('.text-fantasi')
+    this.textMagi = this.shadowRoot.querySelector('.text-magi')
+    this.textPapir = this.shadowRoot.querySelector('.text-papir')
+    this.textTeater = this.shadowRoot.querySelector('.text-teater')
 
-        const templateContent = scrollStoryTemplate.content;
-        this.shadowRoot.appendChild(templateContent.cloneNode(true));
+    const anim = this.setupAnimation().play();
 
-        //******************************************************** */
-        this._config = null;
-    }
 
-    render(val) { }
+    //******************************************************** */
+    this._config = null;
+  }
 
-    attributeChangedCallback(attr, oldval, newval) {
-        if (oldval === newval) return;
-    }
+  render() {
+    this.container.style.backgroundImage = `url(${this._config.backgroundImage})`;
+  }
+
+  attributeChangedCallback(attr, oldval, newval) {
+    if (oldval === newval) return;
+  }
+
+  setupAnimation() {
+    const txtFantasiKFs = [
+      {
+        transform: 'translateX(-50rem)'
+      },
+      {
+        transform: 'translateX(150rem)'
+      }
+    ];
+
+    const txtFantasiFx = new KeyframeEffect(this.textFantasi, txtFantasiKFs, {
+      duration: 3000,
+      fill: "forwards"
+    })
+
+    return new Animation(txtFantasiFx);
+
+  }
+
 
 }
+
+
+
 
 window.customElements.define('ph-scroll-story', ScrollStoryComponent);
--- src/pages/scroll-story/scroll-story.js
+++ src/pages/scroll-story/scroll-story.js
@@ -1,1 +1,10 @@
 import './scroll-story.scss';
+import './../../components/scroll-story/scroll-story.component.js';
+
+import * as BgImage from './../../img/backgrounds/index.js'
+
+
+const phScrollStory = document.querySelector('ph-scroll-story#first');
+phScrollStory.config = {
+    backgroundImage: BgImage.default.Labyrinten
+}
